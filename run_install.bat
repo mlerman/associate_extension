f32a437e1a6b1c886136f99db6941aa3eabba44a
mlerman <mlerman@users.noreply.github.com>
2025-03-18 09:12:05 -0700

    command line to associate .run files

---
+++ run_install.bat
@@ -0,0 +1,2 @@
+c:\UniServer\www\doc\files\ThisPC\nircmd\nircmdc.exe elevate cmd /K "C:\UniServer\www\doc\files\Engineering\ENVIRONMENT\WINDOWS_BATCH\associate_extension\run_install_sub.bat"
+pause
---
+++ run_install_sub.bat
@@ -0,0 +1,4 @@
+@echo off
+assoc .run=runfile
+ftype runfile=c:\UniServer\www\doc\files\Engineering\ENVIRONMENT\WINDOWS_BATCH\associate_extension\callrun.bat "%%1" %%*
+
